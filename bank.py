d80c8bb35c2cfdbf08a989610b1e10c70dbf40ae
spidery804dice <garikmishkin@gmail.com>
2023-11-29 18:00:15 +0300

    Add bank

---
+++ bank.py
@@ -0,0 +1,21 @@
+class Bank:
+	def __init__(self):
+		self.clients = []
+
+	def add_client(self, client):
+		self.clients.append(client)
+
+	def del_client(self, client):
+		for i in range(len(self.clients)):
+			if self.clients[i].id == client.id:
+				self.clients.pop(i)
+				break
+
+	def all_cash(self):
+		res = 0
+		for client in self.clients
+			res += client.money
+
+		return res
+
+
